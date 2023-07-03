a8ccf948b3e39312341e7d9d4a36cdb636a0b3b6
Al-Amin Sarker <alaminsarker5657@gmail.com>
2023-07-03 12:28:18 +0600

    all data loaded

---
+++ src/Pages/Home/AllToys/AllToys.jsx
@@ -0,0 +1,13 @@
+
+const AllToys = () => {
+    return (
+        <div className="mt-8">
+            <div className="text-center">
+                <h2 className="text-5xl font-bold text-orange-500">Here all of our toys!</h2>
+                <p>Our toy shops often stock toys that promote learning and development <br />in areas such as STEM (Science, Technology, Engineering, Mathematics), creativity, language skills, and problem-solving.</p>
+            </div>
+        </div>
+    );
+};
+
+export default AllToys;
